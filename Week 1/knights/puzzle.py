257beddb6bcb8884a106e4777d2e97c6520de348
Sachin Timilsina <sachintimilsina11@gmail.com>
2024-12-28 19:26:45 +0545

    solution to 1-B

--- Week 1/knights/puzzle.py
+++ Week 1/knights/puzzle.py
@@ -9,24 +9,51 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+base_knowledge = And(
+    # It is either Knight or Knave
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+
+    # cannot be both knight and knave
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
+)
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    base_knowledge,
+
+    # If knight states a sentence, then that sentence is true
+    Implication(AKnight, And(AKnight, AKnave)),
+
+    # If a knave states a sentence, then that sentence is false
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    base_knowledge,
+
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    base_knowledge,
+
+    Implication(AKnight, Biconditional(AKnight, BKnight)),
+    Implication(AKnave, Not(Biconditional(AKnave, BKnave))),
+
+    Implication(BKnight, Not(Biconditional(AKnight, BKnight))),
+    Implication(BKnave, Biconditional(AKnave, BKnave)),
 )
 
 # Puzzle 3
@@ -35,7 +62,19 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    base_knowledge,
+
+    Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(AKnave, Not(Or(AKnight, AKnave))),
+
+    Implication(BKnight, AKnave),
+    Implication(BKnave, Not(AKnave)),
+
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight)),
 )
 
 
